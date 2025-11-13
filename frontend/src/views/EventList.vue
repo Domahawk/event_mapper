72c006f9f8177fca308ad28af360f161c47379b6
Domahawk <d.biscan.stu@gmail.com>
2025-11-13 11:27:12 +0100

    Add filters to Event list

--- frontend/src/views/EventList.vue
+++ frontend/src/views/EventList.vue
@@ -12,58 +12,61 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuContent,
-  DropdownMenuItem,
-} from '@/components/ui/dropdown-menu'
-import { Button } from '@/components/ui/button'
 import {router} from "@/router";
+import type {EventFilters} from "@/types/filters.ts";
+import {Button} from "@/components/ui/button";
+import FilterSidebar from "@/components/FilterSidebar.vue";
 
 const events = ref<Event[]>([])
+const filters = ref<EventFilters>({})
+const sidebarOpen = ref<boolean>(false)
 
 const fetchEvents = async (): Promise<void> => {
-  events.value = await eventsApi.all()
+  events.value = await eventsApi.all(filters.value)
+}
+
+const toggleFilters = (): void => {
+  sidebarOpen.value = !sidebarOpen.value
+}
+
+const applyFilters = async (f?: EventFilters): Promise<void> => {
+  filters.value = f ? { ...f } : {}
+  await fetchEvents()
 }
 
 function goToDetail(eventId: number) {
   router.push('/events/' + eventId)
 }
 
-function editEvent(eventId: number) {
-  console.log(`Edit event: ${eventId}`)
-}
-
-function deleteEvent(eventId: number) {
-  console.log(`Delete event: ${eventId}`)
-}
-
 onMounted(fetchEvents)
 </script>
 
 <template>
-  <div class="m-[20px] p-[20px] bg-zinc-900/80 rounded-2xl">
-    <!-- Header -->
-    <div>
-      <h1>Events</h1>
-      <Button>+ Add Event</Button>
+  <div class="w-full flex flex-col items-center">
+    <div class="flex justify-items-start w-full mt-4 px-6">
+      <Button variant="outline" @click="toggleFilters" class="flex items-center gap-2">
+        Filters
+      </Button>
     </div>
 
-    <!-- Table -->
-    <div>
+    <!-- Sidebar -->
+    <FilterSidebar
+        v-model="sidebarOpen"
+        :initial="filters"
+        @apply="applyFilters"
+        @reset="applyFilters"
+    />
+    <div class="m-[20px] p-[20px] bg-zinc-900/80 rounded-2xl w-full md:w-[70%]">
       <Table>
         <TableCaption>List of all upcoming events</TableCaption>
         <TableHeader>
           <TableRow class="flex flex-row">
-            <TableHead class="w-3/4 md:w-1/4 lg:w-1/5">Title</TableHead>
-            <TableHead class="hidden md:block w-1/4 lg:w-1/5">Start</TableHead>
-            <TableHead class="hidden md:block w-1/4 lg:w-1/5">End</TableHead>
-            <TableHead class="hidden lg:block w-1/5">Organizer</TableHead>
-            <TableHead class="w-1/4 md:w-1/4 lg:w-1/5 text-end">Actions</TableHead>
+            <TableHead class="w-1/2 md:w-1/4">Title</TableHead>
+            <TableHead class="md:block w-1/4">Start</TableHead>
+            <TableHead class="hidden md:block w-1/4">End</TableHead>
+            <TableHead class="hidden md:block w-1/4">Organizer</TableHead>
           </TableRow>
         </TableHeader>
-
         <TableBody>
           <TableRow
               v-for="event in events"
@@ -71,49 +74,17 @@ onMounted(fetchEvents)
               @click="goToDetail(event.id)"
               class="flex flex-row w-full"
           >
-            <TableCell class="w-3/4 md:w-1/4 lg:w-1/5 truncate">
+            <TableCell class="w-1/2 md:w-1/4 truncate">
               {{ event.title }}
             </TableCell>
-            <TableCell class="hidden md:block w-1/4 lg:w-1/5">
+            <TableCell class="w-1/2 md:w-1/4">
               {{ dayjs(event.starts_at).format('HH:mm ddd DD/MM') }}
             </TableCell>
-            <TableCell class="hidden md:block w-1/4 lg:w-1/5">
+            <TableCell class="hidden md:block w-1/4">
               {{ dayjs(event.ends_at).format('HH:mm ddd DD/MM') }}
             </TableCell>
-            <TableCell class="hidden lg:block w-1/5">
+            <TableCell class="hidden md:block w-1/4">
               {{ event.organizer.name }}
-            </TableCell>
-
-            <TableCell @click.stop class="flex justify-end w-1/4 md:w-1/4 lg:w-1/5">
-              <DropdownMenu>
-                  <DropdownMenuTrigger as-child>
-                    <Button
-                        size="icon"
-                        variant="ghost"
-                        class="text-neutral-400 hover:text-white hover:bg-neutral-700/30 rounded-full"
-                    >
-                      <span class="text-base leading-none">⋯</span>
-                    </Button>
-                  </DropdownMenuTrigger>
-
-                  <DropdownMenuContent
-                      align="end"
-                      class="w-32 bg-neutral-900 border border-neutral-800 text-neutral-200"
-                  >
-                    <DropdownMenuItem
-                        class="hover:bg-neutral-800 cursor-pointer"
-                        @click="editEvent(event.id)"
-                    >
-                      Edit
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                        class="hover:bg-red-600/20 text-red-400 cursor-pointer"
-                        @click="deleteEvent(event.id)"
-                    >
-                      Delete
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
             </TableCell>
           </TableRow>
         </TableBody>
